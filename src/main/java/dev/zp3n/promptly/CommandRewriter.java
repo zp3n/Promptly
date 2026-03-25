75bc2281db0c9ed89f2c23014ae214d61359d972
zp3n_ <Z94464617@gmail.com>
2026-03-25 18:58:23 +0900

    mini change

--- src/main/java/dev/zp3n/promptly/CommandRewriter.java
+++ src/main/java/dev/zp3n/promptly/CommandRewriter.java
@@ -9,8 +9,44 @@ public class CommandRewriter {
 
     static {
         // ここに変換ルールを書く
+        commandMap.put("/cas", "/warp castle");
+        commandMap.put("/eli", "/warp elizabeth");
+        commandMap.put("/da", "/warp da");
+        commandMap.put("/crp", "/warp crypts");
+        commandMap.put("/mus", "/warp mus");
+        commandMap.put("/tay", "/warp taylor");
+        commandMap.put("/wiz", "/warp wizard_tower");
+        commandMap.put("/car", "/warp carnival");
+        commandMap.put("/sto", "/warp stonks");
+        commandMap.put("/bar", "/warp barn");
+        commandMap.put("/des", "/warp desert");
+        commandMap.put("/tra", "/warp trapper");
+        commandMap.put("/par", "/warp park");
+        commandMap.put("/jun", "/warp jungle");
+        commandMap.put("/how", "/warp howl");
+        commandMap.put("/gal", "/warp galatea");
+        commandMap.put("/mur", "/warp murkwater");
+        commandMap.put("/gol", "/warp gold");
+        commandMap.put("/de", "/warp deep");
+        commandMap.put("/dw", "/warp dwarves");
+        commandMap.put("/for", "/warp forge");
+        commandMap.put("/bas", "/warp basecamp");
+        commandMap.put("/crs", "/warp crystals");
+        commandMap.put("/nuc", "/warp nucleus");
+        commandMap.put("/spi", "/warp spider");
+        commandMap.put("/nes", "/warp top");
+        commandMap.put("/ara", "/warp arachne");
         commandMap.put("/end", "/warp end");
-        commandMap.put("/spawn", "/warp spawn");
+        commandMap.put("/dra", "/warp drag");
+        commandMap.put("/voi", "/warp void");
+        commandMap.put("/net", "/warp nether");
+        commandMap.put("/ku", "/warp kuudra");
+        commandMap.put("/was", "/warp wasteland");
+        commandMap.put("/tai", "/warp dragontail");
+        commandMap.put("/sca", "/warp scarleton");
+        commandMap.put("/smo", "/warp smold");
+        commandMap.put("/rif", "/warp rift");
+        commandMap.put("/bay", "/warp bayou");
 
         // 必要ならどんどん追加
     }
