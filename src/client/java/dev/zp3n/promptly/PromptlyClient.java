edfb2da02e2854c9083f4bedf8fe05df0a458d89
zp3n <137734383+zp3n@users.noreply.github.com>
2026-04-27 00:53:24 +0900

    v0.4 Add HoldBreakClick toggle command

--- src/client/java/dev/zp3n/promptly/PromptlyClient.java
+++ src/client/java/dev/zp3n/promptly/PromptlyClient.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 import dev.zp3n.promptly.config.PromptlyConfig;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvents;
 
 public class PromptlyClient implements ClientModInitializer {
 
@@ -16,6 +18,21 @@ public class PromptlyClient implements ClientModInitializer {
 		ClientSendMessageEvents.ALLOW_COMMAND.register((command) -> {
 
 			String original = "/" + command;
+			if(command.equals("holdbreakclick") || command.equals("hbc")){
+				Minecraft mc = Minecraft.getInstance();
+				if(mc.options.toggleAttack().get()){
+					mc.player.displayClientMessage(Component.literal("[Promptly] Attack/Destroy : Hold"),false);
+					mc.player.playSound(SoundEvents.ARROW_HIT_PLAYER, 0.5F, 0.8F);
+					mc.options.toggleAttack().set(false);
+				}else{
+					mc.player.displayClientMessage(Component.literal("[Promptly] Attack/Destroy : Toggle"), false);
+					mc.player.playSound(SoundEvents.ARROW_HIT_PLAYER, 0.5F, 1.2F);
+					mc.options.toggleAttack().set(true);
+				}
+				mc.options.save();
+				return false;
+			}
+
 			String rewritten = CommandRewriter.rewrite(original);
 
 			if (!original.equals(rewritten)) {
